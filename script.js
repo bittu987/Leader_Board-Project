87f134eeee86fc26cd95c650084a906551f44391
Bittu <bittupathania234@gmail.com>
2024-01-22 19:55:50 +0530

    Completed

--- script.js
+++ script.js
@@ -1,28 +1,155 @@
 
-const submit_Button = document.getElementById("btn");
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+
+    addPlayer(event);
+});
+
+let PlayerList = [];
 
 
-submit_Button.addEventListener('click', (e)=>{
+function addPlayer(event) {
 
-    e.preventDefault();
+    event.preventDefault();
 
-    const players_Data = document.getElementById("playerList");
 
-    const first_Name = document.getElementById("F-N").value;
-    const last_Name = document.getElementById("L-N").value;
-    const country = document.getElementById("ctry").value;
-    const player_Score = document.getElementById("P-Score").value;
+    const playerList = document.getElementById('player-list');
 
-    const list = document.createElement('li');
-    const Name = document.createElement('span');
-    const country_content = document.createElement('span');
-    const score_content = document.createElement('span');
+    const firstName = document.getElementById('first-name').value.toUpperCase();
+    const lastName = document.getElementById('last-name').value.toUpperCase();
+    const country = document.getElementById('country').value.toUpperCase();
+    const score = document.getElementById('score').value.toUpperCase();
+    const nofield = document.getElementById('no-field');
 
-    Name.innerText = first_Name+" "+last_Name;
-    country_content.innerText = country;
-    score_content.innerText = player_Score;
+    if (firstName === '' || lastName === '' || country === '' || score === '') {
+        nofield.innerHTML = 'All fields are required';
+        return;
+    }
 
-    list.append(Name,country_content,score_content);
-    players_Data.append(list);
+    // if(firstName === '' || lastName === '' || country === '' || score === '')
+    // return (LI.style.display = "block");
+    const playerData = {
+        name: firstName + " " + lastName,
+        country: country,
+        score: Number(score)
+    }
 
-})
+    PlayerList.push(playerData);
+    PlayerList.sort((player1, player2) => parseInt(player2.score) - parseInt(player1.score));
+
+    playerList.innerHTML = "";
+
+    for (let index = 0; index < PlayerList.length; index++) {
+
+        let player = PlayerList[index];
+
+        const LI = document.createElement('li');
+        const nameContent = document.createElement('span');
+        const scoreContent = document.createElement('span');
+        const countryContent = document.createElement('span');
+
+
+        const incrementButton = document.createElement('button');
+        const decrementButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        incrementButton.innerHTML = '+5';
+        decrementButton.innerHTML = '-5';
+        deleteButton.innerHTML ='<i class="fa-solid fa-trash"></i>';
+
+        incrementButton.setAttribute('onclick', `increment5(${index})`);
+        decrementButton.setAttribute('onclick', `decrement5(${index})`);
+        deleteButton.setAttribute('onclick', `remove(${index})`);
+
+        nameContent.innerHTML = player.name;
+        countryContent.innerHTML = player.country;
+        scoreContent.innerHTML = player.score
+        
+        LI.append(nameContent, countryContent, scoreContent,deleteButton, incrementButton, decrementButton);
+
+        playerList.appendChild(LI);
+       
+
+    }
+
+}
+
+
+function refreshList() {
+    const playerList = document.getElementById('player-list');
+    PlayerList.sort((player1, player2) => parseInt(player2.score) - parseInt(player1.score));
+
+
+    playerList.innerHTML = "";
+
+    for (let index = 0; index < PlayerList.length; index++) {
+
+        let player = PlayerList[index];
+
+        const LI = document.createElement('li');
+        const nameContent = document.createElement('span');
+        const scoreContent = document.createElement('span');
+        const countryContent = document.createElement('span');
+
+
+        const incrementButton = document.createElement('button');
+        const decrementButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        incrementButton.innerHTML = '+5';
+        decrementButton.innerHTML = '-5';
+        deleteButton.innerHTML ='<i class="fa-solid fa-trash"></i>';
+
+        incrementButton.setAttribute('onclick', `increment5(${index})`);
+        decrementButton.setAttribute('onclick', `decrement5(${index})`);
+        deleteButton.setAttribute('onclick', `remove(${index})`);
+
+        nameContent.innerHTML = player.name;
+        countryContent.innerHTML = player.country;
+        scoreContent.innerHTML = player.score
+        
+        LI.append(nameContent, countryContent, scoreContent,deleteButton, incrementButton, decrementButton);
+
+        playerList.appendChild(LI);
+
+    }
+}
+
+    function increment5(index) {
+
+        PlayerList[index].score += 5;
+        refreshList();
+
+    }
+    function decrement5(index) {
+
+        PlayerList[index].score -= 5;
+        refreshList();
+
+    }
+    function remove(index) {
+
+        delete PlayerList[index];
+        refreshList();
+
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
